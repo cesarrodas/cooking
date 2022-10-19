e60b5843d7dca4167326b570d5345f4d016f27aa
cesar <rodascesar03@yahoo.com>
2022-10-19 11:53:32 -0500

    adding flex to recipe container

--- src/components/RecipeFinder.js
+++ src/components/RecipeFinder.js
@@ -3,13 +3,15 @@ import './RecipeFinder.css';
 const RecipeFinder = ({ recipes }) => {
 
   return (
-    <div className="container">
-      {recipes.map((recipe) => {
-        return <div className="recipe" key={recipe.id}>
-          <img src={recipe.image}/>
-          {recipe.title}
-        </div>
-      })}
+    <div className="parent">
+      <div className="container">
+        {recipes ? recipes.map((recipe) => {
+          return <div className="recipe" key={recipe.id}>
+            <img src={recipe.image}/>
+            {recipe.title}
+          </div>
+        }) : <></>}
+      </div>
     </div>
   )
 }
