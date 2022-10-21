8ce2c376f0528b2a8d41b58bd339caaa8e78875d
cesar <rodascesar03@yahoo.com>
2022-10-20 20:50:26 -0500

    displaying recipe data on recipe detail component

--- src/components/RecipeDetail.js
+++ src/components/RecipeDetail.js
@@ -1,13 +1,38 @@
 import './RecipeDetail.css';
-import { useEffect, useState } from 'react';
+import parse, { attributesToProps } from 'html-react-parser';
 
 const RecipeDetail = ({ recipeData }) => {
 
-  const recipe = () => (
-    <div class="recipeDetail-page">
-      <h1 class="recipeDetail-title">{recipeData.title}</h1>
-    </div>
-  )
+  const recipe = () => {
+
+    let steps = recipeData.analyzedInstructions[0].steps.map((val) => <div><b>Step {val.number}</b><p>{val.step}</p></div>)
+
+    const options = {
+      replace: domNode => {
+        if (domNode.attribs && domNode.name === 'a') {
+          const props = attributesToProps(domNode.attribs);
+          return <b {...props} />;
+        }
+      }
+    };
+
+    return (
+      <div className="recipeDetail-page">
+        <h1 className="recipeDetail-title">{recipeData.title}</h1>
+        <img className="recipeDetail-img" src={recipeData.image} />
+        <div className="recipeDetail-descript">
+          <h2>Summary</h2>
+          <div>{ parse(recipeData.summary, options)}</div>
+          <h2>Instructions</h2>
+          { console.log(steps) }
+          {
+            steps
+          }
+        </div>
+        {/* <div className="recipeDetail-descript">  {parse(recipeData.instructions)}</div> */}
+      </div>
+    )
+  }
 
   return (
     <div className="detail-container">
